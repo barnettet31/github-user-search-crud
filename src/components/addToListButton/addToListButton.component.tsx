527f47563fcb213ba1c4980c6238a90c98e83000
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-10 15:40:37 -0500

    created placeholder button

---
+++ src/components/addToListButton/addToListButton.component.tsx
@@ -0,0 +1,8 @@
+export function AddToListButton (){
+    return (
+      <button className="mt-8 w-full rounded-lg bg-button py-4 px-4 text-center font-bold hover:brightness-150">
+        Add To My List
+      </button>
+    );
+
+}
