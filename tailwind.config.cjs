b3469f7b9653c68ad986eb14ba3e9b02b0aa047e
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-09 21:00:51 -0500

    configured tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'dark-main':"#141d2f",
+        "dark-elevated":"#1e2a47",
+        "main":"#f6f8ff",
+        "elevated":"#fefefe",
+        "button":"#0079ff",
+        "dark-primary":"#ffffff",
+        "dark-secondary":"#ffffff75",
+        "dark-tertiary":"#0079ff",
+        "primary":"#2b3442",
+        "secondary":"#697c9a",
+        "tertiary":"#0079ff"
+      }
+    },
   },
   plugins: [],
 };
