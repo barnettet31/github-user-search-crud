e94e50533448b90169721e3d441b7450bb015786
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-09 22:07:21 -0500

    added use effect to check user preference on load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,51 @@
-import { type AppType } from "next/app";
+import { ReactElement, ReactNode, useEffect } from "react";
+import { AppProps, type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+import { type NextPage } from "next";
+import Head from "next/head";
+// No changes to this type
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
-const MyApp: AppType<{ session: Session | null }> = ({
+// Add generic type
+type AppPropsWithLayout<P> = AppProps<P> & {
+  Component: NextPageWithLayout<P>;
+};
+
+function MyApp({
   Component,
-  pageProps: { session, ...pageProps },
-}) => {
+  pageProps,
+}: AppPropsWithLayout<{ session: Session }>) {
+  const getLayout = Component.getLayout || ((page) => page);
+  useEffect(()=>{
+    const checkDarkMode = function(){
+      if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+      ) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+
+    }
+    checkDarkMode();
+  },[])
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>Front End Mentor - Entertainment App</title>
+        <link rel="shortcut icon" href="/favicon-32x32.png" />
+      </Head>
+      {getLayout(<Component {...pageProps} />)}
     </SessionProvider>
   );
-};
+}
 
 export default api.withTRPC(MyApp);
