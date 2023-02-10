d1b24721233a25c53b9382a71c91abfe90f602af
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-09 22:07:36 -0500

    finalized landing page for simplicity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,27 @@
 import { type GetServerSidePropsContext, type NextPage } from "next";
 import { getSession, signIn } from "next-auth/react";
+import { LandingFooter } from "../components/landingFooter/landingFooter.component";
 
 const Home: NextPage = () => {
 
   return (
-    <main className="min-w-screen grid min-h-screen place-items-center bg-main dark:bg-dark-main">
-      <div className="bg-bg-secondary dark:bg-dark-bg-secondary px-4 py-4 bg-elevated dark:bg-dark-elevated rounded shadow flex flex-col gap-4">
-        <h1 className="text-4xl font-bold" >
-          Welcome
-        </h1>
-        <p className="text-base">This app requires user authentication to operate. Please login.</p>
-        <button onClick={()=>void signIn('google')} className="px-4 py-2 bg-button hover:bg-button/75 rounded shadow">Login With Google</button>
-      </div>
-    </main>
+    <>
+      <main className="min-w-screen grid min-h-screen place-items-center bg-main dark:bg-dark-main">
+        <div className="bg-bg-secondary dark:bg-dark-bg-secondary flex flex-col gap-4 rounded bg-elevated px-4 py-4 shadow dark:bg-dark-elevated">
+          <h1 className="text-4xl font-bold">Welcome</h1>
+          <p className="text-base">
+            This app requires user authentication to operate. Please login.
+          </p>
+          <button
+            onClick={() => void signIn("google")}
+            className="rounded bg-button px-4 py-2 shadow hover:bg-button/75"
+          >
+            Login With Google
+          </button>
+        </div>
+      </main>
+      <LandingFooter />
+    </>
   );
 };
 
