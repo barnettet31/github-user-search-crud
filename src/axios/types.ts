ca31e5e1926f9d25cdc0a99c67874ba85316d0a6
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-10 19:03:39 -0500

    added user type

---
+++ src/axios/types.ts
@@ -0,0 +1,15 @@
+export interface IUserNameResult{
+    avatar_url: string;
+    blog: string;
+    company: string;
+    created_at: string;
+    location: string;
+    public_repos: number;
+    followers: number;
+    following: number;
+    twitter_username: string | null;
+    name: string;
+    login: string;
+    url:string;
+    bio:string;
+}
