bd38f022ebde9670c2fd03e5d2ce0c56d0fe1e8e
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-10 19:06:04 -0500

    added correct props to elements

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,17 +6,58 @@ import { LightDarkToggle } from "../../components/lightDarkToggle/lightDarkToggl
 import { UserSearch } from "../../components/userSearch/userSearch.component";
 import { UserDetails } from "../../components/userDetails/userDetails.component";
 import { AddToListButton } from "../../components/addToListButton/addToListButton.component";
+import { useQuery } from "@tanstack/react-query";
+import { getUserInfo } from "../../axios/getUserInfo";
+import { useState } from "react";
+import { api } from "../../utils/api";
+import { LoadingState } from "../../components/loadingState/loadingState.component";
 const Dashboard: NextPageWithLayout = () => {
-  return (
-    <div>
-      <div className="mx-auto flex w-10/12 flex-col justify-between md:w-9/12 lg:w-full lg:max-w-[730px]">
-        <LightDarkToggle />
-        <UserSearch />
-        <UserDetails/>
-        <AddToListButton/>
-      </div>
-    </div>
+  const [userId, setUserId] = useState("");
+  const [isError, setIsError] = useState(false);
+  const utils = api.useContext()
+  const { data, isLoading:loadingUser} = useQuery(["user", userId], () =>
+    getUserInfo(userId),
+    {
+      enabled: !!userId,
+      onError: () => setIsError(true),
+      retry:0
+    }
   );
+  const {isLoading:addingUser, mutate:addToList} = api.list.setListItem.useMutation({
+    onSettled: ()=>{
+      setUserId("");
+      void utils.list.getListItems.invalidate()
+    }
+  });
+  
+  const updateUserId = (id: string) => {
+    setUserId(id);
+  }
+  const clearError = () => {
+    setIsError(false);
+  }
+  const handleAddToList = ()=>{
+    if(!data) return 
+    addToList({link:data.url});
+  }
+  if (addingUser) return <LoadingState />;
+    return (
+      <div>
+        <div className="mx-auto flex w-10/12 flex-col justify-between md:w-9/12 lg:w-full lg:max-w-[730px]">
+          <LightDarkToggle />
+          <UserSearch
+            clearError={clearError}
+            isError={isError}
+            search={updateUserId}
+          />
+          <UserDetails userData={data} />
+          <AddToListButton
+            isLoading={loadingUser || addingUser}
+            addToList={handleAddToList}
+          />
+        </div>
+      </div>
+    );
 };
 Dashboard.getLayout = getDashboardLayout;
 
