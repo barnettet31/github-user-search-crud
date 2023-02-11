408c69043f1b40b45840df527cd10e79c6b066b0
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-10 19:03:52 -0500

    created button for adding to list

--- src/components/addToListButton/addToListButton.component.tsx
+++ src/components/addToListButton/addToListButton.component.tsx
@@ -1,7 +1,14 @@
-export function AddToListButton (){
+interface IAddToListButton
+{
+    isLoading: boolean;
+    addToList: () => void;
+}
+
+export function AddToListButton ({isLoading, addToList}: IAddToListButton){
+
     return (
-      <button className="mt-8 w-full rounded-lg bg-button py-4 px-4 text-center font-bold hover:brightness-150">
-        Add To My List
+      <button onClick={addToList} disabled={isLoading} className="mt-8 w-full rounded-lg disabled:bg-gray-500 bg-button py-4 px-4 text-center font-bold hover:brightness-150">
+        Add To My Watch List
       </button>
     );
 
