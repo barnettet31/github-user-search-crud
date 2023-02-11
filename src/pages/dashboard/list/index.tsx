14b89a34022edf0bb9ac3e984d2f20d3481a38dd
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-10 19:05:32 -0500

    mapping out list items

--- src/pages/dashboard/list/index.tsx
+++ src/pages/dashboard/list/index.tsx
@@ -2,11 +2,23 @@ import { type GetServerSidePropsContext } from "next";
 import { getSession } from "next-auth/react";
 import { type NextPageWithLayout } from "../../_app";
 import { getDashboardLayout } from "../layout";
+import { api } from "../../../utils/api";
+import { ListItem } from "../../../components/listItem/listItem.component";
+import uuid from "react-uuid";
+import { ListLoadingState } from "../../../components/listLoadingState/listLoadingState.component";
 
 const Dashboard: NextPageWithLayout = () => {
+  const {data, isLoading:gettingItems, isFetching} = api.list.getListItems.useQuery();
+  const {isLoading:deleteItem} = api.list.deleteItem.useMutation()
+  if(gettingItems || isFetching|| deleteItem) return <ListLoadingState/>
+  if(!data) return <h1>No list currently</h1>
   return (
-    <main className="grid min-h-screen min-w-screen place-items-center bg-main dark:bg-dark-main">
-      <h1>This is the list page</h1>
+    <main className="min-h-screen min-w-screen flex flex-col bg-main dark:bg-dark-main">
+      <div className="px-5 py5 grid grid-cols-1 md:grid-cols-2 gap-4">
+        {
+          data.map(url=><ListItem key={uuid()} url={url}/>)
+        }
+      </div>
      
     </main>
   );
