708a1e8241b2bf011b1431d5991c950d86c1959d
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-13 20:51:19 -0500

    added more endpoints for editing lists

--- src/server/api/routers/list.ts
+++ src/server/api/routers/list.ts
@@ -4,33 +4,34 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 export const listRouter = createTRPCRouter({
     setListItem: protectedProcedure.input(
         z.object({
-            link:z.string()
+            listId:z.string(), 
+            link: z.string(), 
         })
     ).mutation(async ({ ctx, input}) =>
     {
         try
         {
-            const myUser = await ctx.prisma.user.findUnique({
+            const myList = await ctx.prisma.list.findUnique({
                 where:{
-                    id:ctx.session.user.id
+                    id:input.listId
                 },
                 select:{
-                    list:true
+                    githubUsers:true
                 }
             });
-            if (!myUser)
+            if (!myList)
             {
                 return console.log("User not found");
             }
-            const alreadyInList = myUser? myUser.list.some(el=>el === input.link) :false ;
+            const alreadyInList = myList.githubUsers.includes(input.link);
             if(alreadyInList) return;
-            const updatedList = myUser? [...myUser.list, input.link] :[input.link];
-            const data = await ctx.prisma.user.update({
+            const updatedList = [...myList.githubUsers, input.link];
+            const data = await ctx.prisma.list.update({
                 where:{
-                    id:ctx.session.user.id
+                    id:input.listId
                 },
                 data:{
-                    list:{
+                    githubUsers:{
                         set:updatedList
                     }
                 }
@@ -42,21 +43,18 @@ export const listRouter = createTRPCRouter({
             console.log("error", error);
         }
     }),
-    getListItems: protectedProcedure.query(async ({ ctx }) =>{
+    getListItems: protectedProcedure.input(z.object({listId:z.string()})).query(async ({ ctx, input }) =>{
         try{
-            const myUser = await ctx.prisma.user.findUnique({
+            const myList = await ctx.prisma.list.findUnique({
                 where:{
-                    id:ctx.session.user.id
+                    id:input.listId
                 },
                 select:{
-                    list:true
+                    githubUsers:true
                 }
             });
-            if (!myUser)
-            {
-                return console.log("User not found");
-            }
-            return myUser.list;
+            if (!myList) return console.log("User not found");
+            return myList.githubUsers;
         }
         catch(e){
             console.log(e)
@@ -64,44 +62,103 @@ export const listRouter = createTRPCRouter({
     } ),
     deleteItem: protectedProcedure.input(
         z.object({
-            link: z.string()
+            listId: z.string(),
+            link: z.string(),
         })
     ).mutation(async ({ ctx, input }) =>
     {
         try
         {
-            const myUser = await ctx.prisma.user.findUnique({
-                where: {
-                    id: ctx.session.user.id
+            const myList = await ctx.prisma.list.findUnique({
+                where:{
+                    id:input.listId
                 },
-                select: {
-                    list: true
+                select:{
+                    githubUsers:true
                 }
             });
-            if (!myUser)
-            {
-                return console.log("User not found");
-            }
-            const alreadyInList = myUser ? myUser.list.some(el => el === input.link) : false;
-            if (!alreadyInList) return;
-            const updatedList = myUser ? myUser.list.filter(el => el !== input.link) :false;
-            if(!updatedList) return console.log("updatedList is empty")
-            const data = await ctx.prisma.user.update({
-                where: {
-                    id: ctx.session.user.id
-                },
-                data: {
-                    list: {
-                        set: updatedList
+            if (!myList) return console.log("User not found");
+            const updatedList = myList.githubUsers.filter((item) => item !== input.link);
+            
+            const myNewList = await ctx.prisma.list.update({
+                where:{id:input.listId},
+                data:{
+                    githubUsers:{
+                        set:updatedList
                     }
                 }
             });
-            return data;
+            return myNewList;
+           
 
         } catch (error)
         {
             console.log("error", error);
         }
     }),
-
-});
+    updateListItem: protectedProcedure.input(z.object({listId:z.string(),  title:z.string(), description:z.string()})).mutation(async ({ ctx, input }) =>{
+        try{
+            const myList = await ctx.prisma.list.update({
+                where:{
+                    id:input.listId
+                },
+                data:{
+                    title:input.title,
+                    description:input.description
+                }
+            });
+            return myList;
+        }
+        catch(e){
+            console.log(e)
+        }
+    }),
+    getCurrentLists: protectedProcedure.query(async ({ ctx }) =>{
+        try{
+            const myList = await ctx.prisma.list.findMany({
+                where:{
+                    userId:ctx.session.user.id
+                },
+                select:{
+                    id:true,
+                    title:true,
+                    description:true
+                }
+            });
+            return myList;
+        }
+        catch(e){
+            console.log(e)
+        }
+    }),
+    createList: protectedProcedure.input(z.object({title:z.string()})).mutation(async ({ ctx, input }) =>{
+        try{
+            const myList = await ctx.prisma.list.create({
+                data:{
+                    title:input.title,
+                    userId:ctx.session.user.id,
+                    description:""
+                }
+            });
+            return myList;
+        }
+        catch(e){
+            console.log(e)
+        }
+    }
+),
+    deleteList: protectedProcedure.input(z.object({listId:z.string()})).mutation(async ({ ctx, input }) =>{
+        try{
+            const myList = await ctx.prisma.list.delete({
+                where:{
+                    id:input.listId
+                }
+            });
+            return myList;
+        }
+        catch(e){
+            console.log(e)
+        }
+    }),
+}
+);
