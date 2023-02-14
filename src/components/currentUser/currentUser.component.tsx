17afa740578db9e301e340b54f3846552c7a77a4
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-02-13 20:51:31 -0500

    loading state

--- src/components/currentUser/currentUser.component.tsx
+++ src/components/currentUser/currentUser.component.tsx
@@ -4,7 +4,7 @@ export const CurrentUser = ()=>{
     const{isLoading, data:me} = api.me.getCurrentUser.useQuery(undefined, {
         retry:false
     });
-    if(isLoading) return <div className="px-4 py-3 w-10 rounded bg-elevated animate-pulse dark:bg-dark-elevated"></div>
+    if(isLoading) return <div className="px-4 py-3 w-10 h-10 rounded bg-elevated animate-pulse dark:bg-dark-elevated"></div>
     return (
       <div className="px-4 py-3">
         <p className="text-sm">Signed in as</p>
